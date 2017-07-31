b09fc68e1ab283fd0ad940047ed519d890798a81
Stangle <RStangle@firstam.com>
2017-07-30 23:01:43 -0400

    Some working code with MANY comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,25 +11,58 @@
 // Letter Bank
 var letterBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var wrongLetters = [];
+	// wrongLetters.push(computerPick); //This did not work either.
+
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guesses = 9;
 // var wrongLetters = blank array that gets filled in
 
-// Computer picks a random letter
-	var computerGuess = letterBank[Math.floor(Math.random()*letterBank.length)];
-		// Checked if working in Console --- yes, computer is picking random letter
-		console.log(computerGuess);
+// Tried to use this to reset the game with a confirm dialog.  Didn't work.
+	// var playAgain = confirm("Wanna Play Again");
+		// if (playAgain) {
+		// 	document.getElementById("#guesses").reset();
+		// }
 
-// Register a user pick
+
+// Computer picks a random letter
+	var computerPick = letterBank[Math.floor(Math.random()*letterBank.length)];
+
+		// Checked if working in Console --- yes, computer is picking random letter
+		console.log(computerPick);
+
+// Register a user pick and Game Begins
 	document.onkeyup = function() {
 		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		// Checked if working in Console --- yes, it is capturing key strokes
+
+		 // Checked if working in Console --- yes, it is capturing key strokes
 		console.log(userGuess);
 
-		if ((computerGuess)===(userGuess)) {
-			//Checked in Console --- yes, the alert is appearing if you pick the same letter
-			alert("YOU WIN!");
+		// Guessing Portion and Main Game Function
+		if ((userGuess)===(computerPick)) {
+			//Checked in Console --- yes, the alert is appearing if you pick the same letter and wins increase
+			wins++;
+			confirm("YOU WIN! Wanna Play Again?"); //How do I make this reset the computer pick, number of guesses, and Used Letter Bank?
+		} else if ((userGuess)!==(computerPick)) {
+			guesses--;
+		} else { 
+			guesses==0;
+			confirm("You Lose. Wanna Play Again");  // This does not work. The guesses continue into the negatives.  I'm so confused.
 		}
+
+		// if (guesses===0) {
+		// 	document.querySelector("#guesses").reset();
+		// }  // This didn't work either.
+	
+
+		var html = "<h1>Guess What Letter I'm Thinking Of</h1>" +
+		"<p>Wins: " + wins + "</p>" +
+		"<p>Losses: " + losses + "</p>" +
+		"<p>Guesses Left: " + guesses + "</p>" +
+		"<p>Your Guesses So Far: " + wrongLetters + "</p>";
+		// Not sure how to make wrongLetters actually work yet.
+
+		document.querySelector("#game").innerHTML = html;
 	}
 		
